6ef8bb7ea92ded383da647fe02ad14dad87d51c5
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-10-12 19:17:26 -0300

    Feat (PORT) : Puerto expresado en main

--- mutant/src/main/java/com/parcial_prog3/mutant/MutantApplication.java
+++ mutant/src/main/java/com/parcial_prog3/mutant/MutantApplication.java
@@ -3,6 +3,8 @@ package com.parcial_prog3.mutant;
 import com.parcial_prog3.mutant.services.MutantDetector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MutantApplication {
@@ -11,6 +13,17 @@ public class MutantApplication {
 		SpringApplication.run(MutantApplication.class, args);
 	}
 
+	@Bean
+	public org.springframework.boot.web.server.WebServerFactoryCustomizer<org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory> containerCustomizer(Environment environment) {
+		return factory -> {
+			String port = environment.getProperty("PORT");
+			if (port != null) {
+				factory.setPort(Integer.parseInt(port));
+			} else {
+				factory.setPort(8080); // Default to port 8080 if PORT is not set
+			}
+		};
+	}
 }
 
 /*
